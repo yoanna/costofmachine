23654bce25a512c6751c03105f4c9c2524d3272e
pl05 <pl05@PL05.fin.com>
2018-03-09 12:46:16 +0100

    upload

---
+++ src/costofmachine/Main.java
@@ -0,0 +1,146 @@
+package costofmachine;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.Color;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
+import com.toedter.calendar.JDateChooser;
+import javax.swing.JButton;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.SwingConstants;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class Main extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Main frame = new Main();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Main() {
+		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBackground(Color.WHITE);
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		
+		JLabel lblZakresDatOd = new JLabel("Zakres dat od");
+		
+		JLabel lblZakresDatDo = new JLabel("Zakres dat do");
+		
+		JDateChooser dateChooser = new JDateChooser();
+		JDateChooser dateChooser_1 = new JDateChooser();
+		
+		JButton btnNewButton = new JButton("Start analizy");
+		btnNewButton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				//countCost.run(from, to, path);
+				Date data_od = dateChooser.getDate();
+				Date data_do = dateChooser_1.getDate();
+				if(data_od == null || data_do==null) {
+					String data_od_tmp; String data_do_tmp;
+					if(data_od == null) {
+						data_od_tmp = "";
+					}
+					else data_od_tmp = format.format(data_od);
+					if(data_do == null) {
+						data_do_tmp = format.format(Calendar.getInstance().getTime());
+					}
+					else
+						data_do_tmp = format.format(data_do);
+					countCost.run(data_od_tmp, data_do_tmp);
+				}
+				else {
+					if(data_od.compareTo(data_do)>0){
+						//jezeli data od jest pozniej niz data do
+						JOptionPane.showMessageDialog(null, "Data pocz¹tku zakresu niepoprawna");
+						dateChooser.setDate(null);
+						
+					}
+					else {
+						countCost.run(format.format(data_od), format.format(data_do));
+					}
+				}
+				
+			}
+		});
+		
+		JLabel lblPlikTekstowyZ = new JLabel("<html> Plik tekstowy z wynikiem analizy zostanie zapisany na dysku sieciowym //dataserver/Logistyka/Analiza maszyn </html>");
+		lblPlikTekstowyZ.setHorizontalAlignment(SwingConstants.CENTER);
+		lblPlikTekstowyZ.setToolTipText("");
+		GroupLayout gl_contentPane = new GroupLayout(contentPane);
+		gl_contentPane.setHorizontalGroup(
+			gl_contentPane.createParallelGroup(Alignment.TRAILING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addGap(96)
+							.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+								.addComponent(lblZakresDatOd)
+								.addComponent(lblZakresDatDo))
+							.addGap(75)
+							.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING)
+								.addComponent(dateChooser_1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+								.addComponent(dateChooser, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)))
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addGap(165)
+							.addComponent(btnNewButton))
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addContainerGap()
+							.addComponent(lblPlikTekstowyZ, GroupLayout.DEFAULT_SIZE, 404, Short.MAX_VALUE)))
+					.addContainerGap())
+		);
+		gl_contentPane.setVerticalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addGap(57)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING)
+						.addComponent(lblZakresDatOd)
+						.addComponent(dateChooser, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+					.addGap(32)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING)
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addComponent(lblZakresDatDo)
+							.addGap(6))
+						.addComponent(dateChooser_1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+					.addGap(26)
+					.addComponent(btnNewButton)
+					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addComponent(lblPlikTekstowyZ, GroupLayout.DEFAULT_SIZE, 52, Short.MAX_VALUE)
+					.addContainerGap())
+		);
+		contentPane.setLayout(gl_contentPane);
+	}
+}
---
+++ src/costofmachine/countCost.java
@@ -0,0 +1,228 @@
+package costofmachine;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Date;
+
+public class countCost {
+	
+	public static void run(String from, String to) {
+		try {
+			Connection conn=DriverManager.getConnection("jdbc:mariadb://192.168.90.123/fatdb","listy","listy1234");
+			PrintStream out;
+			try {
+				out = new PrintStream(new FileOutputStream("\\\\192.168.90.203\\Logistyka\\analiza maszyn\\analiza_maszyn_od_"+from+"_do_"+to+".txt"));
+				System.setOut(out);
+				System.setErr(out);
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			System.out.println("Nr maszyny;Typ;Nazwa;Klient;Cena;Waluta;Data;Material;Workprice;Montage;Construction;Electricians;CNC");
+			//dla maszyn
+			Statement c = conn.createStatement();
+			ResultSet rs2 = c.executeQuery("Select nrMaszyny, opis, typ, klient, cena, waluta, DataKontrakt, komentarz from Calendar where Wyslano = 1 and nrMaszyny like '2/%' and klient is not null and klient <> 'MAGAZYN' and cena <> 0 and DataKontrakt >= '"+from+"' and DataKontrakt <= '"+to+"' order by typ asc, DataKontrakt asc" );
+			while(rs2.next()) {
+				String nrMaszyny = rs2.getString("nrMaszyny");
+				String typ = rs2.getString("typ");
+				String nazwa = rs2.getString("opis");
+				String klient = rs2.getString("klient");
+				String cena = rs2.getString("cena");
+				String waluta = rs2.getString("waluta");
+				String data = rs2.getString("DataKontrakt");
+				String komentarz = rs2.getString("komentarz");
+				
+				//SQLs
+				String sql_a = "select storenotesdetail.PROJECTNUMMER, NRMASZYNY, leverancier, storenotesdetail.artikelcode, artikelomschrijving, storenotesdetail.BESTELD, storenotesdetail.BESTELEENHEID, artikel_kostprijs.MATERIAAL as material, artikel_kostprijs.LONEN as workprice, artikel_kostprijs.cfstandaardeenheid from CALENDAR "
+						+ "join storenotesdetail on storenotesdetail.PROJECTNUMMER like concat(calendar.nrmaszyny, '%') "
+						+ "join artikel_kostprijs on storenotesdetail.artikelcode = artikel_kostprijs.ARTIKELCODE ";
+				String sql_d = "select sum(bestellingdetail.suma), bestellingdetail.munt from bestelling " + 
+						"join bestellingdetail on bestelling.leverancier = bestellingdetail.leverancier and bestelling.ORDERNUMMER = bestellingdetail.ORDERNUMMER ";
+				String sql_b = "select (100*sum(werktijdh)+floor(sum(werktijdm60)/60) + 10*mod(sum(werktijdm60), 60)/6) as montage from werkuren where werkpost NOT IN ('KM01', 'KE01', 'CNC') and (cfproject like '"+nrMaszyny+"%'";
+				String sql_e = "select (100*sum(werktijdh)+floor(sum(werktijdm60)/60) + 10*mod(sum(werktijdm60), 60)/6) as montage from werkuren where werkpost = 'KM01' and (cfproject like '"+nrMaszyny+"%'";
+				String sql_f = "select (100*sum(werktijdh)+floor(sum(werktijdm60)/60) + 10*mod(sum(werktijdm60), 60)/6) as montage from werkuren where werkpost = 'KE01' and (cfproject like '"+nrMaszyny+"%'";
+				String sql_g = "select (100*sum(werktijdh)+floor(sum(werktijdm60)/60) + 10*mod(sum(werktijdm60), 60)/6) as montage from werkuren where werkpost = 'CNC' and (cfproject like '"+nrMaszyny+"%'";
+				
+				if(komentarz.equals("")) {
+					sql_a = sql_a + " where NrMaszyny = '"+nrMaszyny+"' order by projectnummer desc";
+					sql_d = sql_d + " where concat(bestelling.AFDELING, \"/\", bestelling.AFDELINGSEQ) like '"+nrMaszyny+"%' " +
+							"group by munt";
+				}
+				else {
+					sql_a = sql_a + " where NrMaszyny = '"+nrMaszyny+"' or NrMaszyny like '"+komentarz+"' order by projectnummer desc";
+					sql_d = sql_d + " where concat(bestelling.AFDELING, \"/\", bestelling.AFDELINGSEQ) like '"+nrMaszyny+"%' or concat(bestelling.AFDELING, \"/\", bestelling.AFDELINGSEQ) like '"+komentarz+"%' " +
+							"group by munt";
+					sql_b = sql_b + " or cfproject like '"+komentarz+"'";
+					sql_e = sql_e + " or cfproject like '"+komentarz+"'";
+					sql_f = sql_f + " or cfproject like '"+komentarz+"'";
+					sql_g = sql_g + " or cfproject like '"+komentarz+"'";
+				}
+				sql_b = sql_b + ")";
+				sql_e = sql_e + ")";
+				sql_f = sql_f + ")";
+				sql_g = sql_g + ")";
+				
+				double material = 0;
+				double workprice = 0;
+				double montage = 0;
+				double constr = 0;
+				double cnc = 0; 
+				double el = 0;
+				System.out.print(nrMaszyny+";"+typ+";"+nazwa+";"+klient+";"+cena+";"+waluta+";"+data+";");
+				
+				Statement a = conn.createStatement();
+				ResultSet rs = a.executeQuery(sql_a);
+				
+				while(rs.next()){
+					double ilosc = rs.getDouble("besteld");
+					//check unit
+					if(!rs.getString("besteleenheid").equals(rs.getString("cfstandaardeenheid"))) {
+						Statement e = conn.createStatement();
+						ResultSet rs4 = e.executeQuery("Select hoeveelheid from artikel_alteenh where artikelcode = '"+rs.getString("artikelcode")+"' and eenheid = '"+rs.getString("besteleenheid")+"'");
+						while(rs4.next()) {
+							ilosc = ilosc*rs4.getDouble("hoeveelheid");
+						}
+						rs4.close();
+						e.close();
+					}
+					if(rs.getString("artikelcode").startsWith("%") ){
+						double [] price = getPrice(rs.getString("artikelcode"));
+						material+=ilosc*price[0];
+						workprice+=ilosc*price[1];
+						montage+=ilosc*price[2];
+					}
+					else{
+						if(rs.getString("artikelcode").startsWith("KM")) 
+							constr += ilosc*rs.getDouble("workprice");
+						else 
+							workprice+= ilosc*rs.getDouble("workprice");
+						material+=ilosc*rs.getDouble("material");
+						
+					}
+				}
+				rs.close();
+				a.close();
+				
+				//koszt montazu
+				Statement b = conn.createStatement();
+				ResultSet rs1 = b.executeQuery(sql_b);
+				while(rs1.next()) {
+					montage+=rs1.getDouble(1);
+				}
+				rs1.close();
+				b.close();
+				
+				//koszt konstrukcji
+				Statement e = conn.createStatement();
+				ResultSet rs5 = e.executeQuery(sql_e);
+				while(rs5.next()) {
+					constr+=rs5.getDouble(1);
+				}
+				rs5.close();
+				e.close();
+				
+				//koszt elektrykow
+				Statement f = conn.createStatement();
+				ResultSet rs6 = f.executeQuery(sql_f);
+				while(rs6.next()) {
+					el+=rs6.getDouble(1);
+				}
+				rs6.close();
+				f.close();
+				
+				//koszt cnc
+				Statement g = conn.createStatement();
+				ResultSet rs7 = g.executeQuery(sql_g);
+				while(rs7.next()) {
+					cnc+=rs7.getDouble(1);
+				}
+				rs7.close();
+				g.close();
+				
+				//koszt zamowien dodatkowych
+				Statement d = conn.createStatement();
+				
+				ResultSet rs3 = d.executeQuery(sql_d);
+				while(rs3.next()) {
+					if(rs3.getString("munt").equals("EUR")) {
+						material += rs3.getDouble(1)/4.1;
+					}
+					else material += rs3.getDouble(1);
+				}
+				
+				/*System.out.println("Cena materialu "+material);
+				System.out.println("Cena produkcji "+workprice);
+				System.out.println("Cena montazu "+montage);*/
+				System.out.println(String.format( "%.2f",material)+";"+String.format( "%.2f",workprice)+";"+String.format( "%.2f",montage)+";"+String.format( "%.2f",constr)+";"+String.format( "%.2f",el)+";"+String.format( "%.2f",cnc));
+			}
+			
+			conn.close();
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	
+	public static double[] getPrice(String articlecode) throws SQLException{
+		Connection conn=DriverManager.getConnection("jdbc:mariadb://192.168.90.123/fatdb","listy","listy1234");
+		double[] tab = new double[3];
+		//koszt materialow
+		tab[0] = 0; 
+		//koszt produkcji 
+		tab[1] = 0; 
+		//koszt montazu
+		tab[2] = 0;
+				
+		Statement a = conn.createStatement();
+		ResultSet rs = a.executeQuery("Select lonen from artikel_kostprijs where artikelcode = '"+articlecode+"' and soort = 4");
+		while(rs.next()){
+			//System.out.println("Cena montazu "+articlecode+" "+rs.getDouble(1));
+			tab[2]+=rs.getDouble(1);
+		}
+		a.close();
+		rs.close();
+		
+		Statement b = conn.createStatement();
+		ResultSet rs2 = b.executeQuery("select onderdeel, typ, ilosc, jednostka, materiaal as material, lonen as workprice, artikel_kostprijs.cfstandaardeenheid from struktury left join artikel_kostprijs on struktury.onderdeel = artikel_kostprijs.artikelcode where struktury.artikelcode = '"+articlecode+"' and artikel_kostprijs.soort = 4");
+		while(rs2.next()){
+			double ilosc = rs2.getDouble("ilosc");
+			//check unit
+			if(!rs2.getString("jednostka").equals(rs2.getString("cfstandaardeenheid"))) {
+				Statement e = conn.createStatement();
+				ResultSet rs4 = e.executeQuery("Select hoeveelheid from artikel_alteenh where artikelcode = '"+rs2.getString("onderdeel")+"' and eenheid = '"+rs2.getString("jednostka")+"'");
+				while(rs4.next()) {
+					ilosc = ilosc*rs4.getDouble("hoeveelheid");
+				}
+				rs4.close();
+				e.close();
+			}
+			
+			if(rs2.getString("typ").equals("F")||rs2.getString("onderdeel").startsWith("%")){
+				double[] tab_tmp = getPrice(rs2.getString("onderdeel"));
+				tab[0]+=ilosc*tab_tmp[0];
+				tab[1]+=ilosc*tab_tmp[1];
+				
+			}
+			else{
+				tab[0] += ilosc*rs2.getDouble("material");
+				tab[1] += ilosc*rs2.getDouble("workprice"); 
+			}
+		}
+		b.close();
+		rs2.close();
+		conn.close();
+		return tab;
+		
+	}
+	
+
+}
