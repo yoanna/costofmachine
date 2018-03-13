9c134085ccdc74d4d1144f0a8809fb0ed9b4dee1
pl05 <pl05@PL05.fin.com>
2018-03-13 15:22:40 +0100

    dodanie kalkulacji dla projektów od 1 do 50 (bez 2 i 6)

--- src/costofmachine/countCost.java
+++ src/costofmachine/countCost.java
@@ -17,7 +17,7 @@ public class countCost {
 			Connection conn=DriverManager.getConnection("jdbc:mariadb://192.168.90.123/fatdb","listy","listy1234");
 			PrintStream out;
 			try {
-				out = new PrintStream(new FileOutputStream("\\\\192.168.90.203\\Logistyka\\analiza maszyn\\analiza_maszyn_od_"+from+"_do_"+to+".txt"));
+				out = new PrintStream(new FileOutputStream("\\\\192.168.90.203\\Logistyka\\Raporty\\analiza maszyn\\analiza_maszyn_od_"+from+"_do_"+to+".txt"));
 				System.setOut(out);
 				System.setErr(out);
 			} catch (FileNotFoundException e) {
@@ -163,6 +163,124 @@ public class countCost {
 				System.out.println(String.format( "%.2f",material)+";"+String.format( "%.2f",workprice)+";"+String.format( "%.2f",montage)+";"+String.format( "%.2f",constr)+";"+String.format( "%.2f",el)+";"+String.format( "%.2f",cnc));
 			}
 			
+			//dla innych projektow
+			String sql_00 = "Select bestelling.leverancier, leverancier.naam from bestelling "
+					+ "join leverancier on bestelling.leverancier = leverancier.leveranciernr " + 
+					"where bestelling.leverdatum >= '"+from+"' and bestelling.leverdatum <= '"+to+"' and bestelling.leverancier < 50 and bestelling.leverancier not in (2, 6) " + 
+					"group by leverancier";
+			String sql_01 = "Select bestelling.leverancier, bestelling.ordernummer, storenotesdetail.artikelcode, storenotesdetail.besteld, storenotesdetail.besteleenheid, artikel_kostprijs.MATERIAAL as material, artikel_kostprijs.LONEN as workprice, artikel_kostprijs.cfstandaardeenheid from bestelling " + 
+					"join storenotesdetail on bestelling.leverancier = storenotesdetail.afdeling and bestelling.ordernummer = storenotesdetail.afdelingseq " + 
+					"join artikel_kostprijs on storenotesdetail.artikelcode = artikel_kostprijs.artikelcode " + 
+					"where bestelling.leverdatum >= '"+from+"' and bestelling.leverdatum <= '"+to+"' and bestelling.leverancier = ";
+			Statement st00 = conn.createStatement();
+			ResultSet rs00 = st00.executeQuery(sql_00);
+			while(rs00.next()) {
+				String nrGrupy = rs00.getString(1);
+				String nazwa = rs00.getString(1);
+				double material = 0;
+				double workprice = 0;
+				double montage = 0;
+				double constr = 0;
+				double cnc = 0; 
+				double el = 0;
+				System.out.print(nrGrupy+";;"+nazwa+";;;;;");
+				//pobierz cene czesci
+				Statement st01 = conn.createStatement();
+				ResultSet rs01 = st01.executeQuery(sql_01+rs00.getInt(1));
+				while(rs01.next()) {
+					double ilosc = rs01.getDouble("besteld");
+					//check unit
+					if(!rs01.getString("besteleenheid").equals(rs01.getString("cfstandaardeenheid"))) {
+						Statement e = conn.createStatement();
+						ResultSet rs4 = e.executeQuery("Select hoeveelheid from artikel_alteenh where artikelcode = '"+rs01.getString("artikelcode")+"' and eenheid = '"+rs01.getString("besteleenheid")+"'");
+						while(rs4.next()) {
+							ilosc = ilosc*rs4.getDouble("hoeveelheid");
+						}
+						rs4.close();
+						e.close();
+					}
+					if(rs01.getString("artikelcode").startsWith("%") ){
+						double [] price = getPrice(rs01.getString("artikelcode"));
+						material+=ilosc*price[0];
+						workprice+=ilosc*price[1];
+						montage+=ilosc*price[2];
+					}
+					else{
+						if(rs01.getString("artikelcode").startsWith("KM")) 
+							constr += ilosc*rs01.getDouble("workprice");
+						else 
+							workprice+= ilosc*rs01.getDouble("workprice");
+						material+=ilosc*rs01.getDouble("material");
+						
+					}
+					
+				}
+				rs01.close();
+				st01.close();
+				
+				String sql_d = "select sum(bestellingdetail.suma), bestellingdetail.munt from bestelling " + 
+						"join bestellingdetail on bestelling.leverancier = bestellingdetail.leverancier and bestelling.ORDERNUMMER = bestellingdetail.ORDERNUMMER where bestelling.AFDELING= "+nrGrupy+" group by munt";
+				String sql_b = "select (100*sum(werktijdh)+floor(sum(werktijdm60)/60) + 10*mod(sum(werktijdm60), 60)/6) as montage from werkuren where werkpost NOT IN ('KM01', 'KE01', 'CNC') and (cfproject like '"+nrGrupy+"/%')";
+				String sql_e = "select (100*sum(werktijdh)+floor(sum(werktijdm60)/60) + 10*mod(sum(werktijdm60), 60)/6) as montage from werkuren where werkpost = 'KM01' and (cfproject like '"+nrGrupy+"/%')";
+				String sql_f = "select (100*sum(werktijdh)+floor(sum(werktijdm60)/60) + 10*mod(sum(werktijdm60), 60)/6) as montage from werkuren where werkpost = 'KE01' and (cfproject like '"+nrGrupy+"/%')";
+				String sql_g = "select (100*sum(werktijdh)+floor(sum(werktijdm60)/60) + 10*mod(sum(werktijdm60), 60)/6) as montage from werkuren where werkpost = 'CNC' and (cfproject like '"+nrGrupy+"/%')";
+				
+				
+				//koszt montazu
+				Statement b = conn.createStatement();
+				ResultSet rs1 = b.executeQuery(sql_b);
+				while(rs1.next()) {
+					montage+=rs1.getDouble(1);
+				}
+				rs1.close();
+				b.close();
+				
+				//koszt konstrukcji
+				Statement e = conn.createStatement();
+				ResultSet rs5 = e.executeQuery(sql_e);
+				while(rs5.next()) {
+					constr+=rs5.getDouble(1);
+				}
+				rs5.close();
+				e.close();
+				
+				//koszt elektrykow
+				Statement f = conn.createStatement();
+				ResultSet rs6 = f.executeQuery(sql_f);
+				while(rs6.next()) {
+					el+=rs6.getDouble(1);
+				}
+				rs6.close();
+				f.close();
+				
+				//koszt cnc
+				Statement g = conn.createStatement();
+				ResultSet rs7 = g.executeQuery(sql_g);
+				while(rs7.next()) {
+					cnc+=rs7.getDouble(1);
+				}
+				rs7.close();
+				g.close();
+				
+				//koszt zamowien dodatkowych
+				Statement d = conn.createStatement();
+				
+				ResultSet rs3 = d.executeQuery(sql_d);
+				while(rs3.next()) {
+					if(rs3.getString("munt").equals("EUR")) {
+						material += rs3.getDouble(1)/4.1;
+					}
+					else material += rs3.getDouble(1);
+				}
+				
+				/*System.out.println("Cena materialu "+material);
+				System.out.println("Cena produkcji "+workprice);
+				System.out.println("Cena montazu "+montage);*/
+				System.out.println(String.format( "%.2f",material)+";"+String.format( "%.2f",workprice)+";"+String.format( "%.2f",montage)+";"+String.format( "%.2f",constr)+";"+String.format( "%.2f",el)+";"+String.format( "%.2f",cnc));
+			
+			}
+			rs00.close();
+			st00.close();		
 			conn.close();
 			
 		} catch (SQLException e) {
